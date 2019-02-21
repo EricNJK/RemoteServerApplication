7fc84b808757d473d47936645dba0d6f710f05ac
Eric Njoroge <kamau.eric2016@jkuatuni.onmicrosoft.com>
2019-02-21 12:56:45 +0300

    Stage 4.

--- RemoteServerApplication/Program.cs
+++ RemoteServerApplication/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
 using System.Text;
 
 namespace RemoteServerApplication
 {
+    /// <summary>
+    /// .Net Remoting service for AdminApplication.
+    /// </summary>
+    /// 
+    /// <remarks> This console application provides remoting service for the Admin Application. </remarks>
+
     class Program
     {
         static void Main(string[] args)
         {
+            HttpChannel channel = new HttpChannel(50000);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(PTSLibrary.PTSAdminFacade),
+                "PTSAdminFacade", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Press return key to terminate server");
+            Console.ReadLine();
         }
     }
 }
